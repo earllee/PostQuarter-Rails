8743e5c753d0730273625dc6aef285010a61d708
earllee <earlvlee@gmail.com>
2015-11-28 19:48:11 -0500

    Push migrations for previous commit.

---
+++ db/migrate/20151128205600_add_name_to_measurement.rb
@@ -0,0 +1,9 @@
+class AddNameToMeasurement < ActiveRecord::Migration
+  def change
+    add_column :measurements, :name, :string
+    add_column :measurements, :int_value, :integer
+    add_coulmn :measurements, :float_value, :float
+    add_column :measurements, :str_value, :string
+    add_column :measurements, :bool_value, :boolean
+  end
+end
